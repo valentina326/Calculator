982f3ee6bb50adf6e88c330beb985cc745b691ca
Valentina Mala <valentinamala326@gmail.com>
2024-04-18 20:51:21 +0300

    edit String "expression"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String expression = "10/2 / 2/10";
+        String expression = "1/3 + 1/3 * 2/13 - 4/1 / 16/3";
         Calculator calculator = new Calculator(expression);
         System.out.println(calculator.getResult(expression));
     }
